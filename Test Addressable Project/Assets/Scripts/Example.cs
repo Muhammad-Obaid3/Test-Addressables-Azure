a9db5d5235702cf5f7fe1bd81ae9f547bfa86850
Muhammad-Obaid3 <obaidmuhammad305@gmail.com>
2020-12-09 01:48:50 +0500

    After configuring unity addressables

---
+++ Test Addressable Project/Assets/Scripts/Example.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AddressableAssets;
+
+public class Example : MonoBehaviour
+{
+    public AssetReference assetReference;
+
+    void Start()
+    {
+        assetReference.InstantiateAsync();
+    }
+}
